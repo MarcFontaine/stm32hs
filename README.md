29c7146b67f7c0bd681eed91743125922022e82b
MarcFontaine <MarcFontaine@users.noreply.github.com>
2017-11-03 13:59:09 +0100

    add SVD2HS

--- README.md
+++ README.md
@@ -1,13 +1,103 @@
-# STM32Fxxx microcontroller hacking in Haskell
+# STM32Fxxx micro-controller hacking in Haskell
 
 ## STM32-Zombie
+
 The STM32-Zombie library turns a STM32 micro-controller
 into a powerful Haskell-hackable hardware interface.
+It gives the user full control of the STM32Fxxx hardware peripherals
+without the need to write any c-code, without cross-compiler tool chain
+and even without any particular micro controller firmware.
+The library is called STM-32 Zombie because it halts the brain,
+i.e. the ARM CPU of the controller board and instead uses the on-cip-debugging
+features of the controller. The boards run like a Zombie without using
+its own brain.
 
-## STLinkUSB
-This package contains a Haskell driver for ST-Link USB dongles.
-The library is a based on information from the openocd library.
+## Compatible Hardware
+I have tested the library with the following hardware:
+
+* ST-Link USB dongle (Google for "ST-Link V2 stlink mini STM8STM32 STLINK simulator")
+* A STM32F103 breakout board (Google for " "STM32F103C8T6 ARM STM32 Minimum System Development Board Module")
+
+The USB dongle as well as the breakout boards are available in China starting
+at about US$2.
+The cheap ST-link clones and the breakout boards work well but the US$2
+ST-Link clones provide no electrical isolation to protect your PC or Laptop.
+Experimenting with Hardware is a lot of fun until you let the magic smoke out.
+Brand ST-Link boards, that guarantee electrical isolation and full protection
+of the PC, are very affordable and may be a good investing.
+
+## How it Works
+One end of ST-Link dongle plugs into the PC USB port the other end is the so called
+SWD (single wire debug) interface.
+The SWD interface, which also provides electrical power, is connected to the
+STM32F103 breakout with 4 jumper wires.
+As most breakout boards come with a LED, this is all that is needed to run the
+App.Blink example.
+Via the SWD interface the PC can read and write the controller CPU
+address space and access the memory-mapped hardware registers.
+
+## API
+The STM32-Zombie library is modeled after the STMicroelectronics 
+STM32F10x firmware library which provides a low level interface to the 
+controller hardware.
+This API is suitable for bare metal hardware hacking but also allows to build
+higher level interfaces.
+
+## Examples and features
+The STM32Fxxx controllers feature a wide variety of powerful and flexible
+hardware peripherals like GPIO port, serial, SPI, I2C interfaces
+and 12bit-ADC converters and USB ports.
+The killer feature is, that they also include a flexible DMA controller
+that can be uses in combination with the peripherals.
+This makes it possible to build hard-real-time applications that work
+completely independent from the controller CPU.
+Examples are ADC sampling and high sampling rate and with precise timing,
+high frequency sampling of digital inputs or generation of high frequency
+digital output patterns.
+
+The library does not cover all of the STM32 hardware.
+I add support for something when I need it in a particular hardware project.
+(For example I have not tried the USB features of the boards,
+which seems to be very interesting project)
+
+The examples are in the App module hierarchy.
+
+### App.Blink
+Blink a LED.
+### App.ADC
+Buffered ADC converters with DMA transfer.
+### App.DMABuffer
+An example for a serial ports with DMA transfer.
+### App.TestLCD
+An LCD driver.
+This is the original code from the hArduino library
+with some very small adaptions.
+### App.WS1228B
+A driver for nice colorful RGB LED strips.
+(Uses SPI and DMA) 
+### App.Serial
+Hello world example for serial ports.
+
+## Todo
+
+## Support packages
+Parts of the functionality are packaged separately
+
+### STLinkUSB
+The STLinkUSB package contains a Haskell driver for ST-Link USB dongles.
+The library is based on information from the openocd library.
+The STM32-Zombie package only uses a small subset of the ST-Link features
+and only these features of ST-Link protocol are really tested and included
+in STLinkUSB. (There is some extra but untested code). 
 [Haddock documentation](http://hackage.haskell.org/package/STLinkUSB)
 
-## STM32F103xx-SVD
-Names and definitions for STM32F103 registers generated from STM32F103xx.svd
+
+### STM32F103xx-SVD
+This package contains names and definitions for STM32F103 peripherals,
+registers, addresses and the bits of the hardware registers.
+It is generated from STM32F103xx.svd.
+[Haddock documentation](http://hackage.haskell.org/package/STM32F103xx-SVD)
+
+### SVD2HS
+The compiler that translates a file called STM32F103xx.svd to a set of
+Haskell data types and lookup tables.
---
+++ SVD2HS/SVD2HS.hs
@@ -0,0 +1,176 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+module SVD2HS
+where
+import Text.XML.Lens hiding (text)
+import qualified Data.Text as Text
+import Data.Text (Text)
+import qualified Text.XML.Lens as TXL
+import Text.XML(readFile,def)
+import Text.PrettyPrint      
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.Map (Map)
+import qualified Data.Map as Map
+       
+selectPeripheral = root
+                  . el "device"
+                  ./ el "peripherals"
+                  ./ el "peripheral"
+selectPeripheralName = selectPeripheral ./ el "name" . TXL.text
+                 
+selectRegister = selectPeripheral ./ el "registers" ./ el "register"
+selectRegisterName = selectRegister ./ el "name" . TXL.text
+selectField = selectRegister ./ el "fields" ./ el "field"
+selectFieldName = selectField ./ el "name" . TXL.text
+
+svdFile ="STM32F103xx.svd"
+                
+main :: IO ()
+main = do
+  svd <- Text.XML.readFile def svdFile
+  Prelude.writeFile "Device.hs" $ svd2hs svd
+
+svd2hs :: Document -> String
+svd2hs svd = render hsModule
+  where
+     fieldTable =  (Map.fromList
+           $ map (\(register,field,offset,width)
+                  -> ((register,field),(offset,width)))
+           $ concat
+           $ svd ^.. selectRegister
+           . to collectFields)     
+  
+     hsModule = vcat [
+        text "-- Generated from"<+> text svdFile
+       ,text "module Device"
+       ,text "where"
+       ,text "import Data.Word (Word32)"
+       ,blankLine
+
+       ,dataType
+          "Peripheral"
+           (Set.fromList $ svd ^.. selectPeripheralName)
+       ,blankLine
+
+       ,dataType
+          "Register"
+           (Set.fromList $ svd ^.. selectRegisterName)
+       ,blankLine
+
+       ,dataType
+          "Field"
+          (Set.fromList
+           $ map (\(r,f) -> Text.concat [r,"_",f])
+           $ Map.keys fieldTable)
+  
+       ,blankLine
+
+       ,text "peripheralBase :: Peripheral -> Word32"
+       ,funTable
+          "peripheralBase"
+          (svd ^.. selectPeripheral . to foldBaseAddress)
+       ,blankLine
+  
+       ,text "registerOffset :: Peripheral -> Register -> Word32"
+       ,funTable
+          "registerOffset"
+          (concat $ svd ^.. selectPeripheral . to foldRegisterOffset)
+       ,text "--derived peripherals"
+       ,funTable
+          "registerOffset"
+          (svd ^.. selectPeripheral . attributeSatisfies "derivedFrom" (const True)
+          . to derivedPeripheral)
+       ,text "--catch all"
+       ,text "registerOffset p r = error $ show (\"undefined registerOffset\",p ,r)"      ,blankLine
+
+       ,text "fieldToRegister :: Field -> Register"
+       ,funTable
+           "fieldToRegister"
+           (map (\(reg,field)
+                 -> (textText $ Text.concat [reg , "_" , field],textText reg))
+           $ Map.keys fieldTable)
+       ,blankLine
+  
+       ,text "fieldBitOffset :: Field -> Int"
+       ,funTable
+           "fieldBitOffset"
+           (map (\((reg,field),(offset,_))
+                 -> (textText $ Text.concat [reg , "_" , field],textText offset))
+           $ Map.assocs fieldTable)
+       ,blankLine
+  
+       ,text "fieldBitWidth :: Field -> Int"
+       ,funTable
+           "fieldBitWidth"
+           (map (\((reg,field),(_,width))
+                 -> (textText $ Text.concat [reg , "_" , field],textText width))
+           $ Map.assocs fieldTable)
+
+       ]
+
+blankLine :: Doc
+blankLine = text ""
+
+foldBaseAddress :: Element -> (Doc, Doc)          
+foldBaseAddress x
+  = (sel "peripheral" "name" x, sel "peripheral" "baseAddress" x)
+
+derivedPeripheral :: Element -> (Doc,Doc)
+derivedPeripheral p
+  = (sel "peripheral" "name" p <+> text "reg"
+    ,text "registerOffset"
+     <+> (head $ p ^.. el "peripheral" . attr "derivedFrom" . to textText)
+     <+> text "reg"
+    )
+    
+  
+foldRegisterOffset :: Element -> [(Doc, Doc)]
+foldRegisterOffset p
+  = p ^.. el "peripheral" ./ el "registers" ./ el "register" . to fo
+  where
+     peri = sel "peripheral" "name" p
+     fo x= (peri <+> sel "register" "name" x,
+            sel "register" "addressOffset" x)
+
+sel :: Name -> Name -> Element -> Doc
+sel n c p
+  = textText $ selText n c p
+
+selText :: Name -> Name -> Element -> Text
+selText n c p
+  = head $ p ^.. el n ./ el c . TXL.text
+
+collectFields :: Element -> [(Text, Text, Text, Text)]
+collectFields r
+  = r ^.. el "register" ./ el "fields" ./ el "field" . to fo
+  where
+    register = selText "register" "name" r
+    fo x =(register
+          ,selText "field" "name" x
+          ,selText "field" "bitOffset" x
+          ,selText "field" "bitWidth" x)
+  
+
+textText :: Data.Text.Text -> Doc
+textText = text . Text.unpack
+  
+dataType :: String -> Set Text -> Doc
+dataType typeName constructors = vcat [
+    text "data" <+> text typeName
+   ,nest 4 $ vcat $ zipWith (<>) seps $ map textText $ Set.toList $ constructors
+   ,nest 4 $ text "deriving (Show,Eq,Ord)"
+   ]
+   where
+     seps = text "=" : (repeat $ text "|")
+
+funTable :: String -> [(Doc,Doc)] -> Doc
+funTable funName assocs 
+    = vcat $ map mkCase assocs
+  where
+    mkCase (argument, value) = hsep [
+       text funName
+      ,argument
+      ,equals
+      ,value
+      ]
